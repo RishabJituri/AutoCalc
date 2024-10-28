801d9295880628d12001a0891bc24c50ce6ea856
Rishab <rishab.jituri@gmail.com>
2024-10-28 00:20:48 -0400

    first

--- variable.cpp
+++ variable.cpp
@@ -1,20 +1,27 @@
-
+#include <iostream>
+#include <stdexcept>
 
 class variable {
 public: 
     double item;
-    int grad_type;
+    bool grad_type;
     double grad;
-    variable(double value, int gt){
+    variable(double value, bool gt){
         item = value;
         grad_type = gt;
+        grad = 0;
     }
 
     variable __add__(variable one, variable two){}
 };
 
 variable variable::__add__(variable one, variable two){
-    return 
+    variable new_var(one.item + two.item,one.grad_type || two.grad_type);
+    new_var.grad = one.grad + two.grad
+    return new_var;
+
+    }
+    
     
 
 }
